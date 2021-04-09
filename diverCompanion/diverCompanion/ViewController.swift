0596b4d0d0b46d83cda3b3f91b8062e98092a917
mikok42 <mikolaj@linczewski.com>
2021-04-09 14:18:42 +0200

    init

---
+++ diverCompanion/diverCompanion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diverCompanion
+//
+//  Created by Mikołaj Linczewski on 09/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
