c0a50d9637e344731b00bd8cb353b1b19b8e6485
mikok42 <mikolaj@linczewski.com>
2021-04-09 23:28:21 +0200

    .

--- diverCompanion/diverCompanion/ViewController.swift
+++ diverCompanion/diverCompanion/ViewController.swift
@@ -13,46 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var siteLocation: UILabel!
     @IBOutlet weak var siteDescription: UILabel!
     
-    var diveSites = [site1, site2]
+    var diveSites = DiveSite.getData()
     var siteArrayIterator = 0
     
-    func updateUI(){
-        siteImage.image =  #imageLiteral(resourceName:  diveSites[siteArrayIterator].giveData().3)  //tu sie kryje #imageLiteral(resourceName:  diveSites[siteArrayIterator].giveData().pictureName
-        siteName.text =  diveSites[siteArrayIterator].giveData().name
-        siteLocation.text =  diveSites[siteArrayIterator].giveData().location
-        siteDescription.text =  diveSites[siteArrayIterator].giveData().description
+    func updateUI() {
+        siteImage.image = UIImage(named: diveSites[siteArrayIterator].pictureName)
+        siteName.text =  diveSites[siteArrayIterator].name
+        siteLocation.text =  diveSites[siteArrayIterator].location
+        siteDescription.text =  diveSites[siteArrayIterator].description
     }
     override func viewDidLoad() {
         siteDescription.sizeToFit()
         updateUI()
     }
-    
     @IBAction func prevButtonPressed(_ sender: UIButton) {
-        if siteArrayIterator > diveSites.startIndex - 1 {
+        if siteArrayIterator > 0 {
             siteArrayIterator -= 1
         } else {
-            siteArrayIterator = diveSites.endIndex
+            siteArrayIterator = diveSites.count - 1
         }
         updateUI()
     }
     @IBAction func nextButtonPressed(_ sender: UIButton) {
-        if siteArrayIterator < diveSites.endIndex + 1{
-        siteArrayIterator += 1
-        }else {
-            siteArrayIterator = diveSites.startIndex
+        if siteArrayIterator <= diveSites.count - 1 {
+            siteArrayIterator += 1
+        } else {
+            siteArrayIterator = 0
         }
         updateUI()
     }
 }
-
-
-let site1 = diveSite(_name: "Salem Wreck", _location: "Red Sea, Egypt", _description: description1, _pictureName: "salemWreck", _rating: 4)
-let site2 = diveSite(_name: "Daedalus Reef", _location: "Red Sea, Egypt", _description: description2, _pictureName: "daedalusReef", _rating: 5)
-
-let description1 = """
-The Salem Express was a former 115m long passenger ferry which sank in 1991 at great human cost just south of Safaga. The immense tragedy of this event makes diving the Salem Express a unique experience that leaves a lasting impression on most scuba divers. She rests on her starboard side on a flat sand bottom at 27m of depth, with the shallowest section at just 7m.
-"""
-let description2 = """
-Daedalus Reef (also known as Abu Kizan) is a 400-meter-long and 100-meter-wide (1,310–330 ft) standalone reef in the Egyptian Red Sea situated about 90 kilometers from Marsa Alam. There is a small artificial island in the center of the reef, which hosts a lighthouse constructed in 1863 and rebuilt in 1931. Daedalus reef is a well-known place for diving because of good chances to see pelagic fish, such as hammerhead sharks, and an abundance of corals. In the high season one can find many dive safari boats staying overnight, anchored to the reef.
-"""
-
--- diverCompanion/diverCompanion/diveSiteClass.swift
+++ diverCompanion/diverCompanion/diveSiteClass.swift
@@ -5,28 +5,24 @@
 //  Created by Mikołaj Linczewski on 09/04/2021.
 //
 import Foundation
-import UIKit
 
-class diveSite{
-    private
-    let name: String
+struct DiveSite: Codable { //ogarnij
+    private(set) var name: String
     let description: String
     let location: String
-    
     let rating: Int
-   
     let pictureName: String
-    
-    init(_name: String, _location: String, _description: String, _pictureName: String, _rating: Int){
-        name = _name
-        description = _description
-        location = _location
-        rating = _rating
-        pictureName = _pictureName
+ 
+    static func getData() -> [DiveSite] {
+        let description1 = """
+        The Salem Express was a former 115m long passenger ferry which sank in 1991 at great human cost just south of Safaga. The immense tragedy of this event makes diving the Salem Express a unique experience that leaves a lasting impression on most scuba divers. She rests on her starboard side on a flat sand bottom at 27m of depth, with the shallowest section at just 7m.
+        """
+        let description2 = """
+        Daedalus Reef (also known as Abu Kizan) is a 400-meter-long and 100-meter-wide (1,310–330 ft) standalone reef in the Egyptian Red Sea situated about 90 kilometers from Marsa Alam. There is a small artificial island in the center of the reef, which hosts a lighthouse constructed in 1863 and rebuilt in 1931. Daedalus reef is a well-known place for diving because of good chances to see pelagic fish, such as hammerhead sharks, and an abundance of corals. In the high season one can find many dive safari boats staying overnight, anchored to the reef.
+        """
+        return [
+            DiveSite(name: "Daedalus Reef", description: description2, location: "Red Sea, Egypt", rating: 5, pictureName: "daedalusReef"),
+            DiveSite(name: "Salem Wreck", description: description1, location: "Red Sea, Egypt", rating: 4, pictureName: "salemWreck")
+        ]
     }
-    
-    func giveData() ->(name: String, location: String, description: String, pictureName: String, rating: Int){
-        return (name, location, description, pictureName , rating)
-    }
-    
 }
